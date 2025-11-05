92101d2e054e4bf4b46593ed809bd2b7dfadb6e0
Yuverly Hidokun <yuverlyverdezoto@gmail.com>
2025-11-05 16:12:43 -0500

    Reticula cambiante

--- scripts/player/interaction_handler.gd
+++ scripts/player/interaction_handler.gd
@@ -1,44 +1,60 @@
 extends Node
+
 @export var interact_layer = 0b011
 @export var MouseRayCast : Node3D
-@onready var Cursor = get_tree().get_first_node_in_group("cursor")
-#FLAGS
+@export var hud : Node # <- asignado desde el editor, no lo toques en código
+
+
 var can_interact := true
 var current_object
 var already_interacted := false
 
+func _ready():
+	
+	if hud == null:
+		push_warning("⚠️ HUD no asignado en el Inspector. Arrastra el nodo HUD aquí.")
+	else:
+		print("✅ HUD asignado correctamente:", hud.name)
+
 func _process(_delta):
+	if hud == null:
+		return # si no está asignado, no intenta usarlo
+
 	interact()
 	check_if_looking()
+
+var last_interactable = null
 
 func interact():
 	var interaction_ray = MouseRayCast.calc_3D_interactions(interact_layer, 3)
 	if interaction_ray:
 		var collider = interaction_ray.collider
 		if collider.is_in_group("interactable") and can_interact:
-			# ✅ Solo si el jugador presiona "E"
+			if last_interactable != collider:
+				hud.set_crosshair_interact(true)
+				last_interactable = collider
+
 			if Input.is_action_just_pressed("Interact"):
 				collider.mouse_interaction(get_parent())
 				current_object = collider
 				already_interacted = true
-			elif current_object != collider:
-				leave_interaction()
-		elif already_interacted:
-			leave_interaction()
-	elif already_interacted:
-		leave_interaction()
-
+		else:
+			if last_interactable != null:
+				hud.set_crosshair_interact(false)
+				last_interactable = null
+	else:
+		if last_interactable != null:
+			hud.set_crosshair_interact(false)
+			last_interactable = null
+			
 func leave_interaction():
-	#Cursor.decrese_cursor()
-	#if current_object != null and current_object.is_in_group("interactable"):
-	#	current_object.on_mouse_exited()
 	current_object = null
-	#already_interacted = false
-
+	if hud:
+		hud.set_crosshair_interact(false)
 
 func check_if_looking():
 	var interaction_ray = MouseRayCast.calc_3D_interactions(0b011, 20)
 	if interaction_ray:
 		var collider = interaction_ray.collider
 		if collider.is_in_group("lookable"):
-				collider._on_look_event()
+			collider._on_look_event()
