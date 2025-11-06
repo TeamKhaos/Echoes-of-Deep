34da3855860c0d4282bf31e19917d8672f00f364
Mateo Garzon <137083225+WesitosFsa@users.noreply.github.com>
2025-11-05 19:00:08 -0500

    pick up object

--- scripts/palotest.gd
+++ scripts/palotest.gd
@@ -3,7 +3,9 @@ extends RigidBody3D
 @export var item_id: String = "bandage"
 
 func mouse_interaction(player):
-	var object_marker = player.get_node("ObjectMarker")
+	var object_marker = player.object_marker
 	if object_marker:
-		global_transform = object_marker.global_transform
+		get_parent().remove_child(self)
+		object_marker.add_child(self)
+		transform = Transform3D.IDENTITY
 		freeze = true
